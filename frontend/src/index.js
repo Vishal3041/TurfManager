30da2ce222548f7f1da3cc6e90e4097dbe77b03a
Vishal Rajeshkumar Tripathi <vishaltripathi1497@gmail.com>
2026-03-23 03:03:56 +0000

    add google ouath

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="884842209284-jtv2bn0f8cc1hlg5lgoc14isomrk4c2i.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 );
